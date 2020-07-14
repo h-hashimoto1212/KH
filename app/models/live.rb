d7c58b83413a8a9c8c271a80e3862ba1aa5e0435
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-07-15 07:10:14 +0900

    validation and flash notice

--- app/models/live.rb
+++ app/models/live.rb
@@ -3,4 +3,5 @@ class Live < ApplicationRecord
   has_many :details, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: proc { |attributes| attributes['file'].blank? }
   accepts_nested_attributes_for :details, allow_destroy: true
+  validates :title, presence: true
 end
